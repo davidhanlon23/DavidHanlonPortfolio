85d303ac56813ebde7489620d479df72cc26abc5
davidhanlon23 <32404063+davidhanlon23@users.noreply.github.com>
2019-05-02 16:54:46 -0400

    Setting up front end

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports ={
+    //mongoURI:"mongodb+srv://davidhanlon23:password@davidwebportfolio-e4gxa.mongodb.net/test?retryWrites=true",
+   mongoURI:"mongodb+srv://davidhanlon23:$IfYouCanSeeThisPleaseDontHackMe$@davidwebportfolio-e4gxa.mongodb.net/test?retryWrites=true",
+   //$IfYouCanSeeThisPleaseDontHackMe$ 
+    secretOrKey: "secret"
+};
--- mern-auth/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports ={
-    mongoURI:"mongodb+srv://davidhanlon23:$IfYouCanSeeThisPleaseDontHackMe$@davidwebportfolio-e4gxa.mongodb.net/test?retryWrites=true",
-    secretOrKey: "secret"
-};
