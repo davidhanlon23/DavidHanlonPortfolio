82c274b86e67c4aba13532cff9a5e1fc70e84421
davidhanlon23 <32404063+davidhanlon23@users.noreply.github.com>
2019-05-02 02:16:53 -0400

    Set up Backend

---
+++ mern-auth/config/keys.js
@@ -0,0 +1,4 @@
+module.exports ={
+    mongoURI:"mongodb+srv://davidhanlon23:$IfYouCanSeeThisPleaseDontHackMe$@davidwebportfolio-e4gxa.mongodb.net/test?retryWrites=true",
+    secretOrKey: "secret"
+};
