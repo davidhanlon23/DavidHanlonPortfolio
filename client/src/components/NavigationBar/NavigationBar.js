e6377a9166bbc4ef40ddb27934fbdd051af92b35
davidhanlon23 <32404063+davidhanlon23@users.noreply.github.com>
2019-05-13 18:55:47 -0400

    updated login and navigation

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-class Navbar extends Component {
+class NavigationBar extends Component {
   render() {
     return (
       <div className="navbar-fixed">
@@ -22,4 +22,4 @@ class Navbar extends Component {
     );
   }
 }
-export default Navbar;
+export default NavigationBar;
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,31 +1,101 @@
+// import React, { Component } from "react";
+// class Landing extends Component {
+//   render() {
+//     return (
+//       <div style={{ height: "75vh" }} className="container valign-wrapper">
+//         <div className="row">
+//           <div className="col s12 center-align">
+//             <h4>
+//               <b>Build</b> a login/auth app with the{" "}
+//               <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
+//               scratch
+//             </h4>
+//             <p className="flow-text grey-text text-darken-1">
+//               Create a (minimal) full-stack app with user authentication via
+//               passport and JWTs
+//             </p>
+//             <br />
+//             <a
+//               style={{
+//                 width: "150px",
+//                 borderRadius: "3px",
+//                 letterSpacing: "1.5px"
+//               }}
+//               className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+//             >
+//               Register
+//             </a>
+//             <a
+//               style={{
+//                 marginLeft: "2rem",
+//                 width: "150px",
+//                 borderRadius: "3px",
+//                 letterSpacing: "1.5px"
+//               }}
+//               className="btn btn-large waves-effect white hoverable black-text"
+//             >
+//               Log In
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Landing;
+
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { /*Link,*/ withRouter } from "react-router-dom";
+//import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authActions";
+
 class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {
+        isMounted: false
+    };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUpdate = this.componentWillUpdate.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isMounted: true });
+  }
+
+  componentWillUnmount() {
+    this.state.isMounted = false;
+  }
+
+  componentWillUpdate() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   render() {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
-            </p>
-            <br />
-            <a
+            {/* <Link
+              to="/register"
               style={{
                 width: "150px",
                 borderRadius: "3px",
                 letterSpacing: "1.5px"
               }}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3 white-text"
             >
               Register
-            </a>
-            <a
+            </Link> */}
+            <Link
+              to="/login"
               style={{
                 marginLeft: "2rem",
                 width: "150px",
@@ -35,11 +105,21 @@ class Landing extends Component {
               className="btn btn-large waves-effect white hoverable black-text"
             >
               Log In
-            </a>
+            </Link>
           </div>
         </div>
       </div>
     );
   }
 }
-export default Landing;
+Landing.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(withRouter(Landing));
