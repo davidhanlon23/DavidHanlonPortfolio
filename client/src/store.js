1a0bb2709484d24ac4cbeb2c1fb3deb93e1f2a26
davidhanlon23 <32404063+davidhanlon23@users.noreply.github.com>
2019-06-20 17:49:33 -0400

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,23 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+if(process.env.NODE_ENV === 'production') {
+  store = createStore(rootReducer, initialState, compose(
+      applyMiddleware(...middleware)
+  ));
+} else {
+  store = createStore(rootReducer, initialState, compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
+}
 export default store;
