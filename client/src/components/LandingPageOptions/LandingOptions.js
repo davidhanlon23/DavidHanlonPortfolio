ccdc0d49924efd314c6fdb1d1ce93ed0b60b90f4
David Hanlon <david.hanlon@sbdinc.com>
2020-02-05 12:00:17 -0500

    Redesigned Landing Page... not complete

--- client/src/components/LandingPageOptions/LandingOptions.js
+++ client/src/components/LandingPageOptions/LandingOptions.js
@@ -2,7 +2,7 @@ import React /*, { Component }*/ from "react";
 import "animate.css/animate.min.css";
 // import "./LandingOptions.css";
 import ScrollAnimation from 'react-animate-on-scroll';
-import { Button, Typography, Paper } from '@material-ui/core';
+//import { Button, Typography, Paper } from '@material-ui/core';
 import ProductValues from '../LandingPageOptions/ProductValues';
 export default class LandingOptions extends React.Component {
  
@@ -10,54 +10,7 @@ export default class LandingOptions extends React.Component {
     
     return (
       <ScrollAnimation animateIn="fadeIn" animateOnce={true} duration={3}>
-      <section className="page-section" id="hero-banner">
-
-        <div className="row">
-          {/*Landing Option 1 Development */}
-          <div className="column" id="hero-banner-developer">
-            <Paper id="landing-option">
-              <div>
-
-              </div>
-              <Typography className="landing-page-text" variant='body1'>
-                 If you are here looking to view my developer skills, resume, projects, &amp; work experience click the
-                 button below.
-              </Typography>
-              <Button href="/developer" className="landing-page-button" variant="contained"> Developer </Button>
-            </Paper>
-          </div>
-
-          {/*Landing Option 2 Business */}
-          <div className="column" id="hero-banner-entrepreneur">
-            <Paper id="landing-option">
-              <div>
-
-              </div>
-              <Typography className="landing-page-text" variant='body1'>
-                 If you are here looking to view my developer skills, resume, projects, &amp; work experience click the
-                 button below.
-              </Typography>
-              <Button href="/business" className="landing-page-button" variant="contained"> Entrepreneur </Button>
-            </Paper>
-          </div>
-
-          { /*Landing Option 3 Photography */}
-          <div className="column" id="hero-banner-photographer">
-          <Paper id="landing-option">
-              <div>
-
-              </div>
-              <Typography className="landing-page-text" variant='body1'>
-                 If you are here looking to view my developer skills, resume, projects, &amp; work experience click the
-                 button below.
-              </Typography>
-              <Button href="/photography" className="landing-page-button" variant="contained"> Photographer </Button>
-            </Paper>
-          </div>
-
-        </div>
-      </section>
-      <ProductValues/>
+        <ProductValues/>
       </ScrollAnimation>
       
     );
--- client/src/components/LandingPageOptions/ProductValues.js
+++ client/src/components/LandingPageOptions/ProductValues.js
@@ -3,19 +3,27 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import { Button } from "@material-ui/core";
 import Typography from '../Shared/Typography';
+import devSvg from "../../images/html.svg";
+import entrepreneurSvg from "../../images/entrepreneur.svg";
+import amateurPhotographerSvg from "../../images/photographer.svg";
+import landingBg from "../../images/silhouette-of-mountains-1323550.jpg"
 
 const styles = theme => ({
   root: {
     display: 'flex',
     overflow: 'hidden',
-    backgroundColor: theme.palette.secondary.light,
+    backgroundImage: `url(${landingBg})`,
+    backgroundSize: 'cover',
+    // backgroundColor: theme.palette.secondary.light,
   },
   container: {
     marginTop: theme.spacing(15),
     marginBottom: theme.spacing(30),
     display: 'flex',
     position: 'relative',
+   
   },
   item: {
     display: 'flex',
@@ -33,6 +41,7 @@ const styles = theme => ({
   curvyLines: {
     pointerEvents: 'none',
     position: 'absolute',
+    opacity:.4,
     top: -180,
   },
 });
@@ -43,17 +52,17 @@ function ProductValues(props) {
   return (
     <section className={classes.root}>
       <Container className={classes.container}>
-        <img
-          src="/client/public/imgs/computerBg.jpeg"
+        {/* <img
+          src={landingBg}
           className={classes.curvyLines}
           alt="curvy lines"
-        />
+        /> */}
         <Grid container spacing={5}>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
               <img
                 className={classes.image}
-                src="../../images/html.svg"
+                src={devSvg}
                 alt="Dev Icon"
               />
               <Typography variant="h6" className={classes.title}>
@@ -63,13 +72,14 @@ function ProductValues(props) {
                 {'If you are here looking to view my developer skills, resume, projects'}
                 {', & work experience click the button below.'}
               </Typography>
+              <Button href="/developer" className="landing-page-button" variant="contained"> My Resume &amp; Work </Button>
             </div>
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
               <img
                 className={classes.image}
-                src="/client/src/images/entrepreneur.svg"
+                src={entrepreneurSvg}
                 alt="Entrepreneur"
               />
               <Typography variant="h6" className={classes.title}>
@@ -79,13 +89,14 @@ function ProductValues(props) {
                 {'If you are here looking to view my developer skills, resume, projects'}
                 {', & work experience click the button below.'}
               </Typography>
+              <Button href="/business" className="landing-page-button" variant="contained"> My Businesses </Button>
             </div>
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
               <img
                 className={classes.image}
-                src="../../images/photographer.svg"
+                src={amateurPhotographerSvg}
                 alt="Photographer"
               />
               <Typography variant="h6" className={classes.title}>
@@ -95,6 +106,7 @@ function ProductValues(props) {
                 {'If you are here looking to view my developer skills, resume, projects'}
                 {', & work experience click the button below.'}
               </Typography>
+              <Button href="/photography" className="landing-page-button" variant="contained"> My Photos </Button>
             </div>
           </Grid>
         </Grid>
